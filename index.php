f3a536594eb5de72d4eccb675404f4ed87a85910
Daniel Medeiros <danielmmf@gmail.com>
2018-07-12 04:36:35 -0300

    js s

--- index.php
+++ index.php
@@ -214,10 +214,10 @@
 
     <!-- Bootstrap core JavaScript -->
     <script src="js/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for this template -->
     <script src="js/new-age.min.js"></script>
