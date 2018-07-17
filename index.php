8ffabf16879bc3861341aa17097e3413068bf60f
Vogel <11eduardovogel@gmail.com>
2018-07-17 04:14:01 -0300

    buttons modal change

--- index.php
+++ index.php
@@ -135,14 +135,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Tirar uma foto.</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Realizar Denuncia.</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-          <h5>Popover in a modal</h5>
-          <p>This <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
+          <h5>Mapa Local</h5>
+          <!--<p>This <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>-->
           <hr>
           <h5>Tooltips in a modal</h5>
           <p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
