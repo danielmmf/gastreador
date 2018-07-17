d7e7f69b02029116b9344a2cf490af1aeb8dc24e
Daniel Medeiros <danielmmf@gmail.com>
2018-07-17 04:37:57 -0300

    merge

--- index.php
+++ index.php
@@ -131,7 +131,7 @@
  
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"  ng-controller="MarkersEventsAddController">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
