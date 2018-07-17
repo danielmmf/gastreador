faab5e2895f0a0e82210e3f7070f1ae24f380ee8
Vogel <11eduardovogel@gmail.com>
2018-07-17 02:14:57 -0300

    modal change

--- index.php
+++ index.php
@@ -130,6 +130,7 @@
       </div>
 
     </footer>
+ 
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -142,10 +143,14 @@
         </button>
       </div>
       <div class="modal-body">
-        <input type="file" accept="image/*" capture="camera" id="camera">
+          <h5>Popover in a modal</h5>
+          <p>This <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
+          <hr>
+          <h5>Tooltips in a modal</h5>
+          <p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
+          <input type="file" accept="image/*" capture="camera" id="camera">
 <img id="frame" width="100px">
-      
-      </div>
+        </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
